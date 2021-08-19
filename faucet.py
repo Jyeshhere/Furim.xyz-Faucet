0eccd1f85284d6018019dbdfb0e3fdc6107e9d70
Furim <furim@noreply.codeberg.org>
2021-08-19 19:44:52 +0200

    Adding json-API transactions(to make them instant)
    and hash displaying (this panda which u see), also thanks for providing tor/proxy/vpn api

--- faucet.py
+++ faucet.py
@@ -4,16 +4,27 @@ import socket
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask import Flask, request, render_template, jsonify
-### Importing section
+import requests
+import json
+from flask import Flask
+from flask import request
 
+
+### Importing section
+app = Flask(__name__)
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
 
 ### Defining name file as app
-app = Flask(__name__)
 ### Defining name file as app
 
 ### Different system of visiting website
 limiter = Limiter(app, key_func=get_remote_address)
 ### Different system of visiting website
+
+### Defining variables that u see after POST(typing username) request
 
 ### Defining variables that u see after POST(typing username) request
 firstname = str("Duco Faucet ")
@@ -23,8 +34,8 @@ duco_dnt0 = str("DUCO: Faucet_Furimos69")
 ### Defining variables that u see after POST(typing username) request
 
 # Variables with faucet wallet details
-faucetUsername = 'Faucet_Furimos69'
-faucetPassword = 'I_Love_Hentais_Andi_registered_with_duinocoin_account_on_pornhub'
+faucetUsername = ''
+faucetPassword = ''
 msgToSend = 'Furim_Faucet' # Message to send with ducos
 
 ### Code that is executed every time u visit website
@@ -35,41 +46,98 @@ def homepage(): # creating function homepage
 @app.route("/giveMeDucos", methods=['POST']) # Defining request that u can do post(send info)
 @limiter.limit("1/hour") #limitier that limits visiting website [1 requests per hour]
 def giveMeDucos():
-    rand = str(r.uniform(0, 1)) #random variable that define how much u get from duco 
+
+# number1 = 5
+ #   number2 = balance_var
+
+
+
+  #  percent = (number1*number2)/100
+   # print(percent)
+
+
+   # percent = (number1*number2)/100
+   # print(percent)
     
+    #rand = str(r.uniform(0, 0.1)) #random variable that define how much u get from duco 
+#haidangclone    
     # rand = float(0.0000001) # override for debuging
     ducoUsername = request.args.get("ducoUsername")# creating variable that get info from html variable in that case its called fname
 
-    # check if post request (ducoUsername) is not empty
-    if not ducoUsername:
-        return '',400 # returning http code 400 (bad request) if ducoUsername was empty
-    else:
+    if ','  in ducoUsername:
+        return render_template('myes.html')
+
+    if 'Furrymann2578' in ducoUsername:
+        return render_template('myes.html')
+ 
+    if "ArduinoUno" in ducoUsername:
+        return render_template('myes.html')
+
+    if "haidangclone" in ducoUsername:
+        return render_template('myes.html')
+
+
+    if "darestz" in ducoUsername:
+        return render_template('myes.html')
+
+
+    #if "Furrmann2578" in ducoUsername:
+    #           print("Fuck you idiot")
+
+
+    ip = request.environ.get('REMOTE_ADDR')    
+    json_ze_strony = requests.get("http://daiman.nl/check.php?ip={}".format(ip)).json()
+    json_api_username_var = json_ze_strony["code"]
+    print(json_ze_strony)
+    print(json_api_username_var)    
+    str_json_api_username_var = (str(json_api_username_var))
+    if str_json_api_username_var == "100":    
+         return render_template("429.html")
+    else:   
         # here should be try/except to catch timeouts and errors
-
-        server = ("server.duinocoin.com", 2811) # Defining server ip(domain) and port
-        soc = socket.socket() # defining soc which is related socket variable
-
-        soc.connect(server) #connecting to the server using soc variable
-
-        version = soc.recv(5) #Decoding recived info(in that case version of duinocoin)
-        soc.send(bytes("LOGI,{},{}".format(faucetUsername, faucetPassword),encoding="utf8")) #loggin in
-        pong = soc.recv(8) #Decoding recived info(in that case successfully loged in / or not)
+        response_balance = requests.get('https://server.duinocoin.com/balances/{}'.format(faucetUsername))
+        normalized_response_balance = response_balance.text
         
-        soc.send(bytes("SEND,{},{},{}".format(msgToSend, ducoUsername, rand), encoding="utf8")) #Sending funds to someone who gave his username, ducoUsername = Duco username, rand = random amount that person will get 
-        pong1 = soc.recv(8) #Decode it(in that case if sending was sucessfuly or not)
-        pong1d = pong1.decode('utf-8') #Decoding it with utf-8 syntax, aka to recive pure string not (b'verycoolusername')
+        y = json.loads(response_balance.text)
+        not_splitted_var = y["result"]["balance"]
 
-        soc.send(bytes("BALA,{}".format(faucetUsername),encoding="utf8")) # How much duco faucet have
-        pong0 = soc.recv(8) #Decoding it(in that case balance of duinocoin)
-        pong0d = pong0.decode('utf-8') # Decoding it again but in utf-8 syntax???, basically to not receive [b'69.00'] but [69.00]
+        print(not_splitted_var)
 
-        print('[REQUEST] srv version: %s, success? %s, wallet balance: %s, success: %s' % (version, pong, pong0d, pong1d)) # Printing server version, ping response, balance of faucet wallet, if sending duco was successful or not
+
+
+        number1 = float(0.001)
+        number2 = float(not_splitted_var)
+        percent = (number1*number2)/100
+        print(percent)
+
+
+ 
+
+        response = requests.get('https://server.duinocoin.com/transaction/?username={}&password={}&recipient={}&amount={}&memo={}'.format(faucetUsername, faucetPassword, ducoUsername, percent, msgToSend))
+        print(response.text)
+
+
+        y = json.loads(response.text)
+        not_splitted_var = y["result"]
+
+
+        splitted_var = not_splitted_var.split(",")
+
+
+        hash_of_transaction = '''<a style="color:#f5cd79; font-size:40px" href="https://server.duinocoin.com/transactions/{}">🐼</a>'''.format(splitted_var[2])
+
+        print("there is ma boi hash: ", splitted_var[2])
+
+
+
+
+
+        print(not_splitted_var) # Printing server version, ping response, balance of faucet wallet, if sending duco was successful or not
 
         # returning json with sended amount, where and balance
-        return jsonify(ducoSended=rand,
+        return jsonify(ducoSended=percent,
                     ducoSendedTo=ducoUsername,
-                    ducoBalance=pong0d)
-
+                    hash_of_transaction=hash_of_transaction)
 # error 429 aka cooldown website
 @app.errorhandler(429)
 def cooldown(e):
@@ -81,6 +149,9 @@ def cooldown(e):
 if __name__=='__main__':
    app.run()
 # running app function
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0',port=6996)
 
 
 # running app/debuging function
